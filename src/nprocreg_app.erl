55f39d2b9ad2b00f484bb609e7ab7fd45f0fd663
Jesse Gumm <gumm@sigma-star.com>
2020-11-14 18:07:09 -0600

    Simple_cache => nitro_cache

--- src/nprocreg_app.erl
+++ src/nprocreg_app.erl
@@ -10,7 +10,7 @@ start() ->
     start(ignored, []).
 
 start(_Type, StartArgs) ->
-    application:start(simple_cache),
+    application:start(nitro_cache),
     case nprocreg_sup:start_link(StartArgs) of
         {ok, Pid} ->
             {ok, Pid};
